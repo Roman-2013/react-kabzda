4e5a663ec129adf180a19b2a14ba9c7cc8567b82
Roman-2013 <roma.pushkarev.1995@mail.ru>
2023-05-27 19:22:57 +0300

    Commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Accordion} from "./Components/Accordion/Accordion";
+import {Rating} from "./Components/Raiting/Raiting";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    //debugger
+    return (
+        <div>
+            <PageTitle title={"This is APP component"}/>
+            <PageTitle title={"Users frend"}/>
+            <div>Articl1</div>
+            <Rating value={3}/>
+            <Accordion title={"Menu2"} collaps={false}/>
+            <Accordion title={"Menu2"} collaps={true}/>
+            Articl2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+        </div>
+    );
 }
 
 export default App;
