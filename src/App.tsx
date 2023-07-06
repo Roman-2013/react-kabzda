43f102c79ff86b3a6be987a1b6056a1fb1a2607e
Roman-2013 <roma.pushkarev.1995@mail.ru>
2023-07-06 13:34:07 +0300

    lesson 13

--- src/App.tsx
+++ src/App.tsx
@@ -17,48 +17,65 @@ const App = () => {
     const [value, setValue] = useState('')
     const [value1, setValue1] = useState('')
     let count = document.getElementById('idd') as HTMLInputElement
-
-
     const inputRef = useRef<HTMLInputElement>(null)
-
     const save = () => {
         const el = inputRef.current as HTMLInputElement
         setValue1(el.value)
     }
+
+
+    const [parentValue, setparentValue] = useState('')
+    const [parentValue1, setparentValue1] = useState(false)
+    const [parentValue3, setparentValue3] = useState<string|undefined>('1')
+
     return (<div className={'App'}>
 
+            {/*<div>*/}
+            {/*    <input ref={inputRef}/><button onClick={save}>save</button> -actual value: {value1}*/}
+            {/*</div>*/}
+            {/*/!*-----------------------------------------------------------------------*!/*/}
+            {/*<input onChange={(e) => {*/}
+            {/*    setValue(e.currentTarget.value)*/}
+            {/*}}/>-{value}*/}
+            {/*/!*-----------------------------------------------------------------------*!/*/}
+            {/*<div>*/}
+            {/*    <input value={'it-incubator'}/>*/}
+            {/*</div>*/}
+            {/*/!*-----------------------------------------------------------------------*!/*/}
+
+
+            <input value={parentValue} onChange={(e) => setparentValue(e.currentTarget.value)}/>
 
             <div>
-                <input ref={inputRef}/><button onClick={save}>save</button> -actual value: {value1}
+                <input type={'checkbox'} checked={parentValue1}
+                       onChange={(e) => setparentValue1(e.currentTarget.checked)}/>
             </div>
-
-
-
-
-
-
-
-            {/*---------------------*/}
-            <input onChange={(e) => {
-                setValue(e.currentTarget.value)
-            }}/>-{value}
-
-            {/*---------------------*/}
             <div>
-                <input value={'it-incubator'}/>
+                <select value={parentValue3} onChange={(e)=>setparentValue3(e.currentTarget.value)}>
+                    <option value="1">Minsk</option>
+                    <option value="2">Kiev</option>
+                    <option value="3">Moscow</option>
+                </select>
             </div>
 
-
-            {/*<OnOff setonOf={() => setonOf(!onOf)} onOf={onOf}/>{onOf.toString()}*/}
-            {/*<UncontroletOnOff/>*/}
-            {/*<UncontoletAccordionType title={'Menu1'}/>*/}
-            {/*<Accordion title={'Menu2'} collaps={acordion} onChange={() => setacordion(!acordion)}/>*/}
-            {/*<UncontroletRating/>*/}
-            {/*Articl 2*/}
-            {/*<Rating value={raiting} onClick={setraiting}/>*/}
+            {/*<OnOff setonOf={() => setonOf(!onOf)} onOf={onOf}/>{onOf.toString()}*/
+            }
+            {/*<UncontroletOnOff/>*/
+            }
+            {/*<UncontoletAccordionType title={'Menu1'}/>*/
+            }
+            {/*<Accordion title={'Menu2'} collaps={acordion} onChange={() => setacordion(!acordion)}/>*/
+            }
+            {/*<UncontroletRating/>*/
+            }
+            {/*Articl 2*/
+            }
+            {/*<Rating value={raiting} onClick={setraiting}/>*/
+            }
 
         </div>
-    );
+    )
+        ;
 }
 
 
